a23596482057f1ef514ac54b60f54f6a7514864e
RainRaise <114723084+Yu1362531602Qi@users.noreply.github.com>
2025-07-28 19:52:44 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,80 @@
-'use strict';
-const express = require('express');
-const AV = require('leanengine');
+'use strict'
 
-// 加载云函数
-require('./cloud');
+const express = require('express')
+const timeout = require('connect-timeout')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const AV = require('leanengine')
 
-const app = express();
+// Loads cloud function definitions.
+// You can split them into several files, but don't forget to load them into the main file.
+require('./cloud')
 
-// 使用 LeanEngine 中间件
-// 必须在所有自定义路由之前调用
-app.use(AV.express());
+const app = express()
 
-// 允许 express 读取 JSON 格式的请求体
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// Configures template engine.
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-module.exports = app;
+// Configures default timeout.
+app.use(timeout('15s'))
+
+// Loads LeanEngine middleware.
+app.use(AV.express())
+
+app.enable('trust proxy')
+// Uncomment the following line to redirect all HTTP requests to HTTPS.
+// app.use(AV.Cloud.HttpsRedirect())
+
+app.use(express.static('public'))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.get('/', (req, res) => {
+  res.header('Cache-Control', 'no-cache')
+  res.render('index', { currentTime: new Date() })
+})
+
+// You can put routings in multiple files according to their categories.
+app.use('/todos', require('./routes/todos'))
+
+app.use((req, res, next) => {
+  // If there is no routing answering, throw a 404 exception to exception handlers.
+  if (!res.headersSent) {
+    const err = new Error('Not Found')
+    err.status = 404
+    next(err)
+  }
+})
+
+// error handler
+app.use((err, req, res, next) => {
+  if (req.timedout && req.headers.upgrade === 'websocket') {
+    // Ignores websocket timeout.
+    return
+  }
+
+  const statusCode = err.status || 500
+  if (statusCode === 500) {
+    console.error(err.stack || err)
+  }
+  if (req.timedout) {
+    console.error('Request timeout: url=%s, timeout=%d, please check whether its execution time is too long, or the response callback is invalid.', req.originalUrl, err.timeout)
+  }
+  res.status(statusCode)
+  // Do not output exception details by default.
+  let error = {}
+  if (app.get('env') === 'development') {
+    // Displays exception stack on page if running in the development enviroment.
+    error = err
+  }
+  res.render('error', {
+    message: err.message,
+    error: error
+  })
+})
+
+module.exports = app
